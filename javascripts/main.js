449b809b7ee3f8a42e7ff54c55c1fefe8f416501
Mary Alice Orr <m.a.orr17@gmail.com>
2018-04-07 12:53:55 -0500

    added write to dom

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,18 @@
-console.log("hello world");
+
+
+
+
+
+
+// write to DOM function 
+const writeToDom = (domString, divId) => {
+    document.getElementById(divId).innerHTML = domString;
+};
+
+
+// failed function
+
+function nope () {
+    console.log("nope that didn't work");
+}
+
